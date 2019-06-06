e6bd77197bec6cd39e3e5b61d0e56db845714dd4
hanidea <hydavid@126.com>
2019-06-06 09:32:34 +0800

    main.go

--- deployserver/main.go
+++ deployserver/main.go
@@ -17,7 +17,7 @@ import (
 // }
 
 func firstPage(w http.ResponseWriter, r *http.Request){
-	io.WriteString(w,"<h1> Hello, James 0606</h1>")
+	io.WriteString(w,"<h1> Hello, James 06</h1>")
 	//reLaunch()
 }
 
