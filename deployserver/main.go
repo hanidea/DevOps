b6c84ae11668def0079a5056b0fbeaca8bbf54b1
hanidea <hydavid@126.com>
2019-06-06 09:56:20 +0800

    deploy

--- deployserver/main.go
+++ deployserver/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"io"
 	"net/http"
+	"os/exec"
+	"log"
 )
 
+func reLaunch(){
+	cmd := exec.Command("sh","./deploy.sh")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.Wait()
+}
+
 func firstPage(w http.ResponseWriter, r *http.Request){
-	io.WriteString(w,"<h1> Hello, James 5</h1>")
+	io.WriteString(w,"<h1> Hello, James 0606</h1>")
+	reLaunch()
 }
 
 func main() {
