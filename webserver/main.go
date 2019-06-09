5384302b09294c7b43c080cbb8e8cb49a85764e6
hanidea <hydavid@126.com>
2019-06-09 13:20:32 +0800

    test

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request){
-	io.WriteString(w,"<h1> Hello, James devops 0606 again</h1>")
+	io.WriteString(w,"<h1> Hello, James devops 0609</h1>")
 }
 
 func main() {
